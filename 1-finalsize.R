26d565cafc80e864bdae1fefa0985f7306a346f7
Andree Valle <avallecam@gmail.com>
2023-01-16 18:40:22 +0000

    solve vignette 3

--- 1-finalsize.R
+++ 1-finalsize.R
@@ -570,3 +570,201 @@ ggplot(final_size_immunised) +
 
 # vignette 03 -------------------------------------------------------------
 
+# load finalsize
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(finalsize)
+pacman::p_load(socialmixr)
+pacman::p_load(ggplot2)
+
+# get UK polymod data
+polymod <- socialmixr::polymod
+contact_data <- socialmixr::contact_matrix(
+  polymod,
+  countries = "United Kingdom",
+  age.limits = c(0, 5, 18, 40, 65),
+  symmetric = TRUE
+)
+
+# get the contact matrix and demography data
+contact_matrix <- t(contact_data$matrix)
+demography_vector <- contact_data$demography$population
+
+# scale the contact matrix so the largest eigenvalue is 1.0
+contact_matrix <- contact_matrix / max(eigen(contact_matrix)$values)
+
+# divide each row of the contact matrix by the corresponding demography
+contact_matrix <- contact_matrix / demography_vector
+
+n_demo_groups <- length(demography_vector)
+
+# mean R0 is 2.0
+r0_mean <- 2.0
+
+# susceptibility is uniform
+susc_uniform <- matrix(
+  data = 1,
+  nrow = n_demo_groups,
+  ncol = 1L
+)
+
+# p_susceptibility is uniform
+p_susc_uniform <- susc_uniform
+
+# create an R0 samples vector
+r0_samples <- rnorm(n = 1000, mean = r0_mean, sd = 0.1)
+
+hist(r0_samples)
+
+## alternative -------------------------------------------------------------
+
+pacman::p_load(tibble)
+pacman::p_load(dplyr)
+pacman::p_load(tidyr)
+pacman::p_load(purrr)
+pacman::p_load(forcats)
+
+# create a list of arguments
+list_arguments <- list(
+  contact_matrix = contact_matrix,
+  demography_vector = demography_vector,
+  susceptibility = susc_uniform,
+  p_susceptibility = p_susc_uniform
+)
+
+final_size_output <- 
+  
+  # create a dataframe with values from a vector 
+  tibble(r0 = r0_samples) %>% 
+  rownames_to_column() %>% 
+  
+  # map the function to all the r0 values 
+  # with the same set of arguments
+  mutate(temp = map(
+    .x = r0,
+    .f = final_size,
+    contact_matrix = list_arguments$contact_matrix,
+    demography_vector = list_arguments$demography_vector,
+    p_susceptibility = list_arguments$p_susceptibility,
+    susceptibility = list_arguments$susceptibility)) %>% 
+  
+  # unnest all the dataframe outputs in temp
+  unnest(temp) %>% 
+  
+  # relevel the factor variable
+  mutate(demo_grp = fct_relevel(demo_grp,
+                                contact_data %>% 
+                                  pluck("demography") %>% 
+                                  pluck("age.group")))
+
+final_size_output
+
+ggplot(final_size_output) +
+  stat_summary(
+    aes(
+      demo_grp, p_infected
+    ),
+    fun = mean,
+    fun.min = function(x) {
+      quantile(x, 0.05)
+    },
+    fun.max = function(x) {
+      quantile(x, 0.95)
+    }
+  ) +
+  scale_y_continuous(
+    labels = scales::percent,
+    limits = c(0.25, 1)
+  ) +
+  theme_classic() +
+  # theme(
+  #   legend.position = "top",
+  #   legend.key.height = unit(2, "mm"),
+  #   legend.title = ggtext::element_markdown(
+  #     vjust = 1
+  #   )
+  # ) +
+  # coord_cartesian(
+  #   expand = TRUE
+  # ) +
+  labs(
+    x = "Age group",
+    y = "% Infected"
+  )
+
+
+## original ----------------------------------------------------------------
+
+
+# run final size on each sample with the same data
+final_size_data <- Map(
+  r0_samples, seq_along(r0_samples),
+  f = function(r0, i) {
+    # the i-th final size estimate
+    fs <- final_size(
+      r0 = r0,
+      contact_matrix = contact_matrix,
+      demography_vector = demography_vector,
+      susceptibility = susc_uniform,
+      p_susceptibility = p_susc_uniform
+    )
+    
+    fs$replicate <- i
+    fs$r0_estimate <- r0
+    fs
+  }
+)
+
+# combine data
+final_size_data <- data.table::rbindlist(final_size_data)
+
+final_size_data
+
+# order age groups
+final_size_data$demo_grp <- factor(
+  final_size_data$demo_grp,
+  levels = contact_data$demography$age.group
+)
+
+ggplot(final_size_data) +
+  stat_summary(
+    aes(
+      demo_grp, p_infected
+    ),
+    fun = mean,
+    fun.min = function(x) {
+      quantile(x, 0.05)
+    },
+    fun.max = function(x) {
+      quantile(x, 0.95)
+    }
+  ) +
+  scale_y_continuous(
+    labels = scales::percent,
+    limits = c(0.25, 1)
+  ) +
+  theme_classic() +
+  # theme(
+  #   legend.position = "top",
+  #   legend.key.height = unit(2, "mm"),
+  #   legend.title = ggtext::element_markdown(
+  #     vjust = 1
+  #   )
+  # ) +
+  # coord_cartesian(
+  #   expand = TRUE
+  # ) +
+  labs(
+    x = "Age group",
+    y = "% Infected"
+  )
+
+
+
+
+## extra -------------------------------------------------------------------
+
+# final_size(r0 = r0_mean,
+#            contact_matrix = list_arguments$contact_matrix,
+#            demography_vector = list_arguments$demography_vector,
+#            p_susceptibility = list_arguments$p_susceptibility,
+#            susceptibility = list_arguments$susceptibility)
