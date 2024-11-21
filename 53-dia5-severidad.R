5d34ff495573bdbd274450784cddd226407486ee
Andree Valle Campos <avallecam@gmail.com>
2024-11-21 12:09:24 +0000

    add covid360 solutions

--- 53-dia5-severidad.R
+++ 53-dia5-severidad.R
@@ -47,12 +47,23 @@ cfr::cfr_static(
 )
 
 
+# ebola complete ----------------------------------------------------------
+
+ebola_full <- cfr::ebola1976
+
+cfr::cfr_static(data = ebola_full)
+
+cfr::cfr_static(
+  data = ebola_full,
+  delay_density = function(x) density(ebola_delay, x)
+)
+
 # rolling ebola -----------------------------------------------------------------
 
-ebola_rolling_naive <- cfr::cfr_rolling(data = ebola60)
+ebola_rolling_naive <- cfr::cfr_rolling(data = ebola_full)
 
 ebola_rolling_adjusted <- cfr::cfr_rolling(
-  data = ebola60,
+  data = ebola_full,
   delay_density = function(x) density(ebola_delay, x)
 )
 
@@ -130,6 +141,71 @@ covid_rolling_naive <- cfr::cfr_rolling(data = covid60)
 
 covid_rolling_adjusted <- cfr::cfr_rolling(
   data = covid60,
+  delay_density = function(x) density(covid_delay, x)
+)
+
+# bind by rows both output data frames
+bind_rows(
+  covid_rolling_naive %>%
+    mutate(method = "naive"),
+  covid_rolling_adjusted %>%
+    mutate(method = "adjusted")
+) %>%
+  # visualise both adjusted and unadjusted rolling estimates
+  ggplot() +
+  geom_ribbon(
+    aes(
+      date,
+      ymin = severity_low,
+      ymax = severity_high,
+      fill = method
+    ),
+    alpha = 0.2, show.legend = FALSE
+  ) +
+  geom_line(
+    aes(date, severity_estimate, colour = method)
+  )
+
+
+# compare delay distributions ---------------------------------------------
+
+plot(ebola_delay, xlim = c(0,60))
+mean(ebola_delay)
+plot(covid_delay, xlim = c(0,60))
+mean(covid_delay)
+
+density(covid_delay,at = 0:20) %>% enframe()
+
+
+# covid 360 ---------------------------------------------------------------
+
+covid360 <- read_rds("https://epiverse-trace.github.io/tutorials-middle/data/covid_360days.rds")
+
+## incidence2 ------------------------------------------------------
+
+covid360 %>% 
+  incidence2::incidence(
+    date_index = "date",
+    counts = c("cases","deaths"),
+    complete_dates = TRUE
+  ) %>% 
+  plot()
+
+## static ------------------------------------------------------
+
+cfr::cfr_static(data = covid360)
+
+cfr::cfr_static(
+  data = covid360,
+  delay_density = function(x) density(covid_delay, x)
+)
+
+## rolling ------------------------------------------------------
+
+covid_rolling_naive <- cfr::cfr_rolling(data = covid360)
+
+covid_rolling_adjusted <- cfr::cfr_rolling(
+  data = covid360,
   delay_density = function(x) density(covid_delay, x)
 )
 
