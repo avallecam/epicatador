52b6a546884d57672f43f7fdacb2d25ac39323db
Andree Valle Campos <avallecam@gmail.com>
2025-06-05 15:47:04 +0100

    add hexwall code using hexsession

---
+++ 89-hexsession.R
@@ -0,0 +1,23 @@
+
+hexsession::make_tile(
+  packages = c(
+    "readr",
+    "rio",
+    "dplyr",
+    "simulist",
+    "cleanepi",
+    "magrittr",
+    "epiCo",
+    "linelist",
+    "ggplot2",
+    "epiparameter",
+    "cfr",
+    "serofoi",
+    "fitdistrplus",
+    "superspreading",
+    "epichains",
+    "epidemics",
+    "finalsize",
+    "vaccineff"
+  )
+)
