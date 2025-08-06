af3ff3e581cbe18e35b24abb1e922349dea47a84
Andree Valle Campos <avallecam@gmail.com>
2025-08-06 16:07:12 +0100

    add hexsession for tutorials

---
+++ 89-hexsession-tutorials.R
@@ -0,0 +1,17 @@
+
+hexsession::make_tile(
+  packages = c(
+    "cleanepi",
+    "linelist",
+    "incidence2",
+    "simulist",
+    "epiparameter",
+    "cfr",
+    "EpiNow2",
+    "superspreading",
+    "epichains",
+    "socialmixr",
+    "epidemics",
+    "finalsize"
+  )
+)
